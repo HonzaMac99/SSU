53dd6ca1d39e681f964788ea72ae8d1c388ecef1
macaljan_WinPC <macaljan@fel.cvut.cz>
2022-12-06 12:20:56 +0100

    add task pdf

--- 2_BackProp/mlp/softmax_layer.py
+++ 2_BackProp/mlp/softmax_layer.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 class SoftmaxLayer(object):
     def __init__(self, name):
         super(SoftmaxLayer, self).__init__()
@@ -17,4 +15,9 @@ class SoftmaxLayer(object):
         return X
 
     def delta(self, Y, delta_next):
-        return delta_next*()
+        norm = 0
+        for i in range(len(Y)):
+            norm += np.exp(Y[i])
+        for i in range(len(Y)):
+            Y[i] = (Y[i]*norm - Y[i])/(norm**2)
+        return delta_next.T@Y
