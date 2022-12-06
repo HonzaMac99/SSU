1d9cba32380c10de18c07b7620725fcc7fe21815
macaljan_WinPC <macaljan@fel.cvut.cz>
2022-12-06 12:48:50 +0100

    replace for cycles with numpy operations

--- 2_BackProp/mlp/relu_layer.py
+++ 2_BackProp/mlp/relu_layer.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class ReLULayer(object):
     def __init__(self, name):
         super(ReLULayer, self).__init__()
@@ -7,9 +9,10 @@ class ReLULayer(object):
         return False
 
     def forward(self, X):
-        return max(0, X)
+        return np.maximum(0, X)
 
     def delta(self, Y, delta_next):
         # the derivative is undef in Y=0
         #  -> choosed 1 if Y=0
-        return delta_next*(1 if Y >= 0 else 0)
+        delta_Y = np.array([(1 if Y[i] >= 0 else 0) for i in range(len(Y))])
+        return delta_next.T@delta_Y
--- 2_BackProp/mlp/softmax_layer.py
+++ 2_BackProp/mlp/softmax_layer.py
@@ -7,17 +7,11 @@ class SoftmaxLayer(object):
         return False
 
     def forward(self, X):
-        norm = 0
-        for i in range(len(X)):
-            norm += np.exp(X[i])
-        for i in range(len(X)):
-            X[i] = np.exp(X[i])/norm
+        norm = np.sum(np.exp(X))
+        X = np.exp(X)/norm
         return X
 
     def delta(self, Y, delta_next):
-        norm = 0
-        for i in range(len(Y)):
-            norm += np.exp(Y[i])
-        for i in range(len(Y)):
-            Y[i] = (Y[i]*norm - Y[i])/(norm**2)
+        norm = np.sum(np.exp(X))
+        Y = (Y*norm - Y)/(norm**2)
         return delta_next.T@Y
